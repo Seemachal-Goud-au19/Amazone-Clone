5fbdd46fe06250e532f6a437d5827f40b005a307
Seemachal Goud <80570045+Seemachal-Goud-au19@users.noreply.github.com>
2021-12-03 13:42:37 +0530

    Add files via upload

---
+++ build/precache-manifest.35002bf45b3812b88bf2cfa616dd1bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2eb7f6bdd9aa4bfc90faa31decd3a4",
+    "url": "/Amazone-Clone/index.html"
+  },
+  {
+    "revision": "01e9853fc7941be6bb58",
+    "url": "/Amazone-Clone/static/css/main.a8654140.chunk.css"
+  },
+  {
+    "revision": "3eec613caf6b98a337f2",
+    "url": "/Amazone-Clone/static/js/2.fe29b776.chunk.js"
+  },
+  {
+    "revision": "befc7210e8d600b7724f6d6a2fb68035",
+    "url": "/Amazone-Clone/static/js/2.fe29b776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c83a063d964b9cce86",
+    "url": "/Amazone-Clone/static/js/3.2cd09e87.chunk.js"
+  },
+  {
+    "revision": "01e9853fc7941be6bb58",
+    "url": "/Amazone-Clone/static/js/main.39d8b0b5.chunk.js"
+  },
+  {
+    "revision": "5b8f2d156b7d782f93bb",
+    "url": "/Amazone-Clone/static/js/runtime-main.5d26d172.js"
+  }
+]);
